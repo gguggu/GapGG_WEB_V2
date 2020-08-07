c477dc36953b423400c22e62e806a3efbbd38656
gguggu <adrw1364@naver.co>
2020-08-07 16:21:35 +0900

    [ADD] ADD NotFound
    낫파운드 컴포넌트 추가

---
+++ src/component/common/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <div className="NotFound-title">NOT FOUND | 404</div>
+    </div>
+  );
+};
+
+export default NotFound;
