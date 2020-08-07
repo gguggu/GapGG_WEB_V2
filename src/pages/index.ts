a1a226fb15fe44e567003f5b7398a89f75d02634
gguggu <adrw1364@naver.co>
2020-08-07 16:22:23 +0900

    [ADD] ADD Pages
    페이지들 추가

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as NotFoundPage } from './NotFoundPage';
+export { default as ComparePage } from './ComparePage';
+export { default as SearchPage } from './SearchPage';
+export { default as HomePage } from './HomePage';
