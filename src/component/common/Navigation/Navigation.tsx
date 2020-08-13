b4e8ca1b04d52cf664c03ad1696ffc7adbe1b45f
gguggu <adrw1364@naver.co>
2020-08-13 17:05:18 +0900

    [FIX] FIX Navigation
    Navigation 퍼블리싱 작업

--- src/component/common/Navigation/Navigation.tsx
+++ src/component/common/Navigation/Navigation.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import LOGO from 'assets/images/logos/Gap.GG Logo_White_Scaled.png';
+import './Navigation.scss';
 
 const Navigation = () : JSX.Element => {
   return (
     <div className="Navigation">
       <Link className="Navigation-logo" to={'/'}>
-        {/* <img src={LOGO} alt="logo"/> */}
+        <img src={LOGO} alt="logo"/>
       </Link>
     </div>
   );
