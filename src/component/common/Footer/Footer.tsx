6ca908a44166459a56b929500facc72a4022d07e
gguggu <adrw1364@naver.co>
2020-08-13 17:03:46 +0900

    [FIX] FIX Footer
    Footer의 퍼블리싱 작업을 했습니다.

--- src/component/common/Footer/Footer.tsx
+++ src/component/common/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Footer.scss';
 
-const Footer = () => {
+const Footer = () : JSX.Element => {
   return (
     <div className="Footer">
-    <div className="Footer-box">
-      <div className="Footer-box-title">© GAP.GG. All Rights Reserved</div>
+      <div className="Footer-box">
+        <div className="Footer-box-title">© GAP.GG. All Rights Reserved</div>
         <div className="Footer-box-desc">
           gap.gg isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
         </div>
